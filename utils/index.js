9d27c3958e3d80ff63e96ce90fe56f79e3abdaf9
fershad <27988517+fershad@users.noreply.github.com>
2025-01-03 16:00:20 +0800

    remove zone check

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-export const zoneTypeCheck = (zone) => {
-  if (typeof zone !== "string" && typeof zone !== "object") {
-    return {
-      status: "error",
-      message: "Invalid zone. Zone must be a string or object.",
-    };
-  }
-}
