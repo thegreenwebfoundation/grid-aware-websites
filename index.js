71cadb626530a5d5602ead13e9717701ad7eee1b
fershad <27988517+fershad@users.noreply.github.com>
2025-02-20 14:50:29 +0800

    define PowerBreakdwon class

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { GridIntensity } from "./lib/gridIntensity.js";
-import { fetchPowerBreakdown } from "./lib/powerBreakdown.js";
+import { PowerBreakdown } from "./lib/powerBreakdown.js";
 
 export {
   GridIntensity,
-  fetchPowerBreakdown as powerBreakdown,
+  PowerBreakdown
 };
